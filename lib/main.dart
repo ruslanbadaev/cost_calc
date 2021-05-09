27b4ed94d8ff672dd3f8a324d68d7dddab9d0447
ruslanbadaev <r.badaeff@gmail.com>
2021-05-09 23:25:31 +0300

    route animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:flutter_cost_calc/notifiers/card_notifier.dart';
 import 'package:flutter_cost_calc/notifiers/theme_notifier.dart';
 import 'package:flutter_cost_calc/screens/vote_screen.dart';
@@ -30,9 +31,13 @@ class MyApp extends StatefulWidget {
 class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     final darkTheme = ThemeData(
-      textTheme: GoogleFonts.comfortaaTextTheme(
-        Theme.of(context).textTheme,
+      textTheme: GoogleFonts.comfortaaTextTheme(textTheme).copyWith(
+        body1: GoogleFonts.comfortaa(
+            textStyle: textTheme.body1, color: Colors.white, fontSize: 18),
+        body2: GoogleFonts.comfortaa(
+            textStyle: textTheme.body2, color: Colors.white, fontSize: 24),
       ),
       primarySwatch: Colors.grey,
       primaryColor: Colors.black,
@@ -44,24 +49,42 @@ class MyAppState extends State<MyApp> {
     );
 
     final lightTheme = ThemeData(
-      textTheme: GoogleFonts.comfortaaTextTheme(
-        Theme.of(context).textTheme,
+      textTheme: GoogleFonts.comfortaaTextTheme(textTheme).copyWith(
+        body1: GoogleFonts.comfortaa(
+            textStyle: textTheme.body1, color: Colors.black, fontSize: 18),
+        body2: GoogleFonts.comfortaa(
+            textStyle: textTheme.body2, color: Colors.black, fontSize: 24),
       ),
+      buttonTheme: ButtonThemeData(
+          buttonColor: Colors.deepPurpleAccent,
+          colorScheme: ColorScheme.light(background: Colors.red)),
       primarySwatch: Colors.grey,
       primaryColor: Colors.white,
       brightness: Brightness.light,
-      backgroundColor: const Color(0xFFE5E5E5),
-      accentColor: Colors.black,
-      accentIconTheme: IconThemeData(color: Colors.white),
+      backgroundColor: Colors.grey[500],
+      accentColor: Colors.deepPurpleAccent,
+      accentIconTheme: IconThemeData(color: Colors.deepPurpleAccent),
       dividerColor: Colors.white54,
+      buttonColor: Colors.deepPurpleAccent,
     );
     ThemeNotifier themeNotifier = Provider.of<ThemeNotifier>(context);
     return MaterialApp(
       title: 'Port Card',
       theme: themeNotifier.get() ? darkTheme : lightTheme,
       home: HomeScreen(title: 'Port Card'),
-      routes: {
-        "/add_scanner": (context) => CardScreen(),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/calc_screen':
+            return PageTransition(
+              child: CardScreen(),
+              type: PageTransitionType.topToBottom,
+              duration: Duration(milliseconds: 800),
+              settings: settings,
+            );
+            break;
+          default:
+            return null;
+        }
       },
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_button/flutter_button.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:flutter_cost_calc/components/card/card_view.dart';
 import 'package:flutter_cost_calc/notifiers/card_notifier.dart';
 import 'package:flutter_cost_calc/notifiers/theme_notifier.dart';
@@ -38,7 +39,7 @@ class _MyHomePageState extends State<HomeScreen> {
             IconButton(
               icon: Icon(Icons.add),
               onPressed: () {
-                Navigator.pushNamed(context, "/add_scanner");
+                Navigator.pushNamed(context, "/calc_screen");
               },
             ),
             IconButton(
@@ -51,35 +52,66 @@ class _MyHomePageState extends State<HomeScreen> {
         ),
         body: cardNotifier != null
             ? Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                    Text(
-                      'Нет ничего приятнее, чем узнать Вашу экономию с помощью наших услуг',
-                      style: TextStyle(color: Colors.white38, fontSize: 24),
-                    ),
-                    SizedBox(
-                      height: 18,
-                    ),
-                    Text('От этого Вас удерживает только эта манящая кнопка',
-                        style: TextStyle(color: Colors.white70, fontSize: 18)),
-                    SizedBox(
-                      height: 24,
-                    ),
-                    AnimePressButton(
-                      borderRadius: BorderRadius.circular(100),
-                      color: Colors.orange,
-                      onTap: () {},
-                      wGradient: false,
-                      /*         gradientColors: [
-                        Colors.black,
-                        Colors.green,
-                      ], */
-                      title: "Рассчитать",
-                      titleColor: Colors.white,
-                    ),
-                  ]))
+                child: Card(
+                    child: Container(
+                        padding: EdgeInsets.all(56),
+                        height: 500,
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                  'Нет ничего приятнее, чем узнать Вашу экономию с помощью наших услуг',
+                                  style: Theme.of(context).textTheme.body2),
+                              SizedBox(
+                                height: 24,
+                              ),
+                              Text(
+                                  'От этого Вас удерживает только эта манящая кнопка',
+                                  style: Theme.of(context).textTheme.body1),
+                              SizedBox(
+                                height: 32,
+                              ),
+                              /*               AnimePressButton(
+                                borderRadius: BorderRadius.circular(200),
+                                color: Colors.deepPurpleAccent,
+                                onTap: () {
+                                  print(123);
+                                },
+                                wGradient: false,
+                                title: "Рассчитать",
+                                titleColor: Colors.white,
+                              ), */
+                              Button3D(
+                                height: 100,
+                                width: 240,
+                                style: StyleOf3dButton(
+                                    z: 16,
+                                    backColor: Colors.deepPurple,
+                                    topColor: Colors.deepPurpleAccent),
+                                onPressed: () {
+                                  print('tap');
+                                  Navigator.pushNamed(context, "/calc_screen");
+                                },
+                                child: Text(
+                                  "Расчитать",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 24),
+                                ),
+                              ),
+                              /*            AnimatedHoverButton(
+                                borderRadius: BorderRadius.circular(100),
+                                tappedTitleColor: Colors.white,
+                                spashColor: Colors.deepPurpleAccent,
+                                titleColor: Colors.deepPurpleAccent,
+                                borderColor: Colors.deepPurpleAccent,
+                                title: "Рассчитать",
+                                titleSize: 24,
+                                onTap: () {
+                               
+                                },
+                              ), */
+                            ]))))
             : Center(
                 child: CircularProgressIndicator(),
               ));
